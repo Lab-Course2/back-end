86abcd97c9803cd367597496e8219952b229046f
lizableta <bletaeliza@gmail.com>
2024-06-30 01:40:19 +0200

    Planet and Satellite

---
+++ MindMuse.AspNetCore/Controllers/PlanetController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MindMuse.Data.Contracts.Models;
+using MindMuse.Data.Data;
+
+namespace MindMuse.AspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanetController : ControllerBase
+    {
+        private readonly MindMuseContext _context;
+
+        public PlanetController(MindMuseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Planet>>> GetPlanets()
+        {
+            return await _context.Planets.ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "GetPlanet")]
+        public async Task<ActionResult<Planet>> GetPlanet(int id)
+        {
+            var planet = await _context.Planets.FindAsync(id);
+            if (planet == null)
+            {
+                return NotFound();
+            }
+            return planet;
+        }
+
+        [HttpPost("CreatePlanet")]
+        public async Task<ActionResult<Planet>> CreatePlanet(Planet planet)
+        {
+            _context.Planets.Add(planet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("GetPlanet", new { id = planet.Id }, planet);
+        }
+
+        
+        [HttpDelete("DeletePlanet{id}")]
+        public async Task<IActionResult> DeletePlanet(int id)
+        {
+            var planet = await _context.Planets.FindAsync(id);
+            if (planet == null)
+            {
+                return NotFound();
+            }
+
+            _context.Planets.Remove(planet);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        [HttpPut("UpdatePlanetById/{id}")]
+        public async Task<IActionResult> UpdatePlanet(int id, Planet planet)
+        {
+            if (id != planet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(planet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlanetExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        private bool PlanetExist(int id)
+        {
+            return _context.Planets.Any(e => e.Id == id);
+        }
+    }
+
+}
---
+++ MindMuse.Data.Contracts/Models/Planet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindMuse.Data.Contracts.Models
+{
+    public class Planet
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Type { get; set; }
+        public bool isDeleted {  get; set; }
+    }
+}
